92530022b5e116066eca558cc10b5181e203a6cf
Edgar Alcolea <edgaralcolea@gmail.com>
2022-02-16 20:41:36 +0100

    Added query params extraction

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const debug = require("debug")("node-calculator");
 const http = require("http");
+const url = require("url");
 const { first, second } = require("./numbers");
 let { port } = require("./numbers");
 const { getNumbers } = require("./getNumbers");
@@ -15,14 +16,16 @@ server.listen(port, () => {
   debug(`Server is up in http://localhost:${port}`);
 });
 server.on("error", (error) => {
-  debug("Error on server");
+  debug(`Error on server: ${error.message}`);
 });
 server.on("request", (request, response) => {
   debug(`Request arrived at ${request.url} with method ${request.method}`);
-
+  const paramsObject = url.parse(request.url, true).query;
+  console.log(paramsObject);
   if (request.url === "/calculator") {
     response.statusCode = 200;
     response.write("<h1>Welcome to Node Calculator</h1>");
+    response.write(`<div><h2>Resultados:</h2>`);
   } else {
     response.statusCode = 404;
     response.write("<h1>Resource not found</h1>");
