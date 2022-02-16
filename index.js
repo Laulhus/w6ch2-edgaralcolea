d7f595a52f2a298baadd5f0db6830203d1c6ffc5
Edgar Alcolea <edgaralcolea@gmail.com>
2022-02-16 20:23:01 +0100

    Created server

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
 require("dotenv").config();
 const debug = require("debug")("node-calculator");
+const http = require("http");
 const { first, second } = require("./numbers");
+let { port } = require("./numbers");
 const { getNumbers } = require("./getNumbers");
 const { printResults } = require("./printResults");
+
+if (typeof port === "undefined") {
+  port = process.env.DEFAULT_PORT;
+}
+
+const server = http.createServer();
+server.listen(port, () => {
+  debug(`Server is up in http://localhost:${port}`);
+});
+server.on("error", (error) => {
+  debug("Error on server");
+});
+server.on("request", (request, response) => {
+  debug(`Request arrived at ${request.url} with method ${request.method}`);
+
+  if (request.url === "/calculator") {
+    response.statusCode = 200;
+    response.write("<h1>Welcome to Node Calculator</h1>");
+  } else {
+    response.statusCode = 404;
+    response.write("<h1>Resource not found</h1>");
+  }
+  response.end();
+});
 
 if (!first && !second) {
   getNumbers();
--- numbers.js
+++ numbers.js
@@ -2,8 +2,10 @@ const { program } = require("commander");
 
 program.option("-f, --first <number>");
 program.option("-s, --second <number>");
+program.option("-p, --port <number>");
 program.parse();
 
 const { first, second } = program.opts();
+const { port } = program.opts();
 
-module.exports = { first, second };
+module.exports = { first, second, port };
