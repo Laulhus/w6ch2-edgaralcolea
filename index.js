1ba5599806e790ee876adf783f3d8798ac17d0cc
Edgar Alcolea <edgaralcolea@gmail.com>
2022-02-16 21:41:19 +0100

    Added error handling when params are not numbers

--- index.js
+++ index.js
@@ -21,11 +21,33 @@ server.on("error", (error) => {
 server.on("request", (request, response) => {
   debug(`Request arrived at ${request.url} with method ${request.method}`);
   const paramsObject = url.parse(request.url, true).query;
-  console.log(paramsObject);
+  const firstNum = +paramsObject.first;
+  const secondNum = +paramsObject.second;
   if (request.url === "/calculator") {
     response.statusCode = 200;
     response.write("<h1>Welcome to Node Calculator</h1>");
-    response.write(`<div><h2>Resultados:</h2>`);
+    response.write(
+      "<h2>Introduce two numbers as query params to get results.</h2>"
+    );
+  } else if (request.url.includes("?")) {
+    if (Number.isNaN(firstNum) || Number.isNaN(secondNum)) {
+      response.statusCode = 500;
+      response.write("<h1>Error: Missing one of the parameters</h1>");
+    } else {
+      response.write(`<div><h2>Resultados:</h2>`);
+      response.write(
+        `<p>${+firstNum} + ${+secondNum} = ${+firstNum + +secondNum}</p>`
+      );
+      response.write(
+        `<p>${+firstNum} - ${+secondNum} = ${+firstNum - +secondNum}</p>`
+      );
+      response.write(
+        `<p>${+firstNum} * ${+secondNum} = ${+firstNum * +secondNum}</p>`
+      );
+      response.write(
+        `<p>${+firstNum} / ${+secondNum} = ${+firstNum / +secondNum}</p>`
+      );
+    }
   } else {
     response.statusCode = 404;
     response.write("<h1>Resource not found</h1>");
